0e406604f1fd0ed8b55aa410991e9658ec01995f
Nagynys <nagyny1@gmail.com>
2024-04-19 12:58:45 +0200

    Blikač

---
+++ blikac.js
@@ -0,0 +1,13 @@
+﻿// Funkce timeru
+const zmenBarvu = () => {
+	const r = Math.round(Math.random() * 255)
+	const g = Math.round(Math.random() * 255)
+	const b = Math.round(Math.random() * 255)
+	document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b +")"
+}
+
+// Načtení stránky
+window.onload = () => {
+	setInterval(zmenBarvu, 2000)
+	zmenBarvu()
+}
